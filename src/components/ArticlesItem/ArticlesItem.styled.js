9acd52cb09868bedbd5a1a7a87140ec4e77c2eca
Olena-Rudnieva <rudnievao@gmail.com>
2024-02-09 08:39:41 +0200

    Add image

--- src/components/ArticlesItem/ArticlesItem.styled.js
+++ src/components/ArticlesItem/ArticlesItem.styled.js
@@ -3,21 +3,37 @@ import { theme } from 'constants/theme';
 
 export const ArticlesItemWrapper = styled.li`
   position: relative;
-  width: 310px;
+  width: 260px;
   border-radius: 15px;
   padding: 50px 10px 10px 10px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  ${theme.mq.tablet} {
+    width: 280px;
+  }
+  ${theme.mq.desktop} {
+    width: 310px;
+  }
 `;
 
 export const ImageWrapper = styled.div`
-  width: 280px;
-  height: 200px;
+  width: 210px;
+  height: 210px;
   box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
   border-radius: 15px;
   overflow: hidden;
   margin-bottom: 28px;
   margin-left: auto;
   margin-right: auto;
+
+  ${theme.mq.tablet} {
+    width: 240px;
+    height: 240px;
+  }
+  ${theme.mq.desktop} {
+    width: 280px;
+    height: 200px;
+  }
 `;
 
 export const Image = styled.img`
